6487c20f5bb71d3b165788fca6254465716c19bd
pedrofgodinho <pedro.fernandez.godinho@gmail.com>
2025-09-01 18:36:49 +0100

    Add multiline print test

--- src/vga_buffer/tests.rs
+++ src/vga_buffer/tests.rs
@@ -1,5 +1,5 @@
 use crate::println;
-use crate::vga_buffer::{BUFFER_HEIGHT, WRITER};
+use crate::vga_buffer::{BUFFER_HEIGHT, BUFFER_WIDTH, WRITER};
 
 #[test_case]
 fn test_println_simple() {
@@ -20,5 +20,29 @@ fn test_println_output() {
     for (i, c) in s.chars().enumerate() {
         let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
         assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
+
+#[test_case]
+fn test_println_output_multiline() {
+    let s = "Some test string that goes beyond the width of the screen. This is some really long test!\nSome test string that does not go beyond the width of the screen.";
+
+    println!("{}", s);
+
+    let mut row = BUFFER_HEIGHT - 4;
+    let mut col = 0;
+    for c in s.chars() {
+        if c == '\n' {
+            row += 1;
+            col = 0;
+            continue;
+        }
+        let screen_char = WRITER.lock().buffer.chars[row][col].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+        col += 1;
+        if col >= BUFFER_WIDTH {
+            row += 1;
+            col = 0;
+        }
     }
 }
